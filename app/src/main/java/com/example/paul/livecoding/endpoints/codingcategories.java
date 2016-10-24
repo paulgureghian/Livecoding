71befe13e3aa7b85caaab810218f6ba4b9edaa1a
Paul <paulgureghian@att.net>
2016-10-24 15:41:19 -0700

    10-24-16   3:00 PM

--- app/src/main/java/com/example/paul/livecoding/endpoints/codingcategories.java
+++ app/src/main/java/com/example/paul/livecoding/endpoints/codingcategories.java
@@ -1,5 +1,9 @@
 package com.example.paul.livecoding.endpoints;
 
+import com.example.paul.livecoding.pojo.Categories;
+
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -7,7 +11,7 @@ import retrofit2.http.Query;
 public interface codingcategories {
 
     @GET ("/api/codingcategories")
-    Call
+    Call <List<Categories>> getData;
 
 
 
---
+++ app/src/main/java/com/example/paul/livecoding/pojo/Categories.java
@@ -0,0 +1,13 @@
+package com.example.paul.livecoding.pojo;
+
+
+
+public class Categories {
+
+String url;
+String name;
+String sort;
+
+
+
+}
