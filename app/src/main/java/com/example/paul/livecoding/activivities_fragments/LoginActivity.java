1c90d7d95e7201766495548a1dad52c7b5c1cf96
Paul <paulgureghian@att.net>
2016-11-02 17:27:33 -0700

    11-02-16  5:15    PM

--- app/src/main/java/com/example/paul/livecoding/activivities_fragments/LoginActivity.java
+++ app/src/main/java/com/example/paul/livecoding/activivities_fragments/LoginActivity.java
@@ -3,7 +3,13 @@ package com.example.paul.livecoding.activivities_fragments;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -19,28 +25,159 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import com.example.paul.livecoding.R;
+import com.example.paul.livecoding.oauthentication.AccessToken;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
-public class LoginActivity extends AppCompatActivity  {
+public class LoginActivity extends AppCompatActivity {
+
+    private static String RANDOM_STATE = "random_state_string";
+    private static String CLIENT_ID = "kBadIfKZYpgLTowhMreDXJ5ckyGz7t8BxUdIJ2XC";
+    private static String CLIENT_SECRET = "yfhYWhsXkjct9R2bfHYVMGcdEQWNT7Plc99MYx98ejlJpc90Mj2hky3ADOPWeyTZz43KrjGrkQLEPUawkZnsmFfxm8RZQzqVZuQ4SxtdISBrDAqbjt1OWuv60LEBDL7R";
+    private static String REDIRECT_URI = "http://localhost/externalapp";
+    private static String GRANT_TYPE = "implicit";
+    private static String OAUTH_TOKEN_URL = "https://www.livecoding.tv/o/authorize/?client_id=" + CLIENT_ID + "&response_type=token&" + RANDOM_STATE;
+
+    WebView web;
+    Button auth;
+    SharedPreferences pref;
+    TextView Access;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        pref = getSharedPreferences("AppPref", MODE_PRIVATE);
+        Access = (TextView) findViewById(R.id.Access);
+        auth = (Button) findViewById(R.id.auth);
+        auth.setOnClickListener(new View.OnClickListener() {
+            Dialog auth_dialog;
+
+            @Override
+            public void onClick(View view) {
+
+                Log.e("URL", OAUTH_TOKEN_URL + "?redirect_uri=" + REDIRECT_URI + "&response_type=token&client_id" + CLIENT_ID);
+                auth_dialog = new Dialog(LoginActivity.this);
+                auth_dialog.setContentView(R.layout.auth_dialog);
+                web = (WebView) auth_dialog.findViewById(R.id.webv);
+                web.getSettings().setJavaScriptEnabled(true);
+                web.loadUrl(OAUTH_TOKEN_URL + "?redirect_uri=" + REDIRECT_URI + "&response_type=token&client_id" + CLIENT_ID);
+                web.setWebViewClient(new WebViewClient() {
+
+                    boolean authComplete = false;
+                    Intent resultIntent = new Intent();
+
+                    @Override
+                    public void onPageStarted(WebView view1, String url, Bitmap favicon) {
+                        super.onPageStarted(view1, url, favicon);
+                        Log.e("page_started_url", url);
+                    }
+
+                    String authCode;
+
+                    @Override
+                    public void onPageFinished(WebView view1, String url) {
+                        super.onPageFinished(view1, url);
+
+                        if (url.contains("?code=") && authComplete != true) {
+                            Uri uri = Uri.parse(url);
+                            authCode = uri.getQueryParameter("code");
+                            Log.i("", "CODE: " + authCode);
+                            authComplete = true;
+                            resultIntent.putExtra("code", authCode);
+                            LoginActivity.this.setResult(Activity.RESULT_OK, resultIntent);
+                            setResult(Activity.RESULT_CANCELED, resultIntent);
+
+                            SharedPreferences.Editor edit = pref.edit();
+                            edit.putString("Code", authCode);
+                            edit.commit();
+                            auth_dialog.dismiss();
+                            new TokenGet().execute();
+                            Toast.makeText(getApplicationContext(), "Authorization code is: " + authCode, Toast.LENGTH_SHORT).show();
+                        } else if (url.contains("error_acccess_denied")) {
+                            Log.i("", "ACCESS_DENIED_HERE");
+                            resultIntent.putExtra("code", authCode);
+                            authComplete = true;
+                            setResult(Activity.RESULT_CANCELED, resultIntent);
+                            Toast.makeText(getApplicationContext(), "error occured", Toast.LENGTH_SHORT).show();
+
+                            auth_dialog.dismiss();
+                        }
+                    }
+                });
+                auth_dialog.show();
+                auth_dialog.setTitle("Authorize Livecoding");
+                auth_dialog.setCancelable(true);
+            }
+        });
+    }
+
+    private class TokenGet extends AsyncTask<String, String, JSONObject> {
+        private ProgressDialog pDialog;
+        String Code;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(LoginActivity.this);
+            pDialog.setMessage("Contacting Livecoding...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            Code = pref.getString("Code", "");
+            pDialog.show();
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... args) {
+
+            AccessToken jParser = new AccessToken();
+            JSONObject json = jParser.gettoken(OAUTH_TOKEN_URL, Code, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, GRANT_TYPE);
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            pDialog.dismiss();
+            if (json != null) {
+                try {
+                    String tok = json.getString("access_token");
+                    String expire = json.getString("expires_in");
+                    String refresh = json.getString("refresh_token");
+
+                    Log.d("Token Access", tok);
+                    Log.d("Expire", expire);
+                    Log.d("Refresh", refresh);
+                    auth.setText("Authenticated");
+                    Access.setText("Access Token:" + tok + "nExpires:" + expire + "nRefresh Token:" + refresh);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(getApplicationContext(), "Network Error", Toast.LENGTH_SHORT).show();
+                pDialog.dismiss();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/paul/livecoding/activivities_fragments/MainActivity.java
+++ app/src/main/java/com/example/paul/livecoding/activivities_fragments/MainActivity.java
@@ -51,20 +51,8 @@ import static com.example.paul.livecoding.R.id.toolbar;
 
 public class MainActivity extends AppCompatActivity implements Callback<List<LiveStreamsOnAir>> {
 
-    private static String RANDOM_STATE = "random_state_string";
-    private static String CLIENT_ID = "kBadIfKZYpgLTowhMreDXJ5ckyGz7t8BxUdIJ2XC";
-    private static String CLIENT_SECRET = "yfhYWhsXkjct9R2bfHYVMGcdEQWNT7Plc99MYx98ejlJpc90Mj2hky3ADOPWeyTZz43KrjGrkQLEPUawkZnsmFfxm8RZQzqVZuQ4SxtdISBrDAqbjt1OWuv60LEBDL7R";
-    private static String REDIRECT_URI = "http://localhost/externalapp";
-    private static String GRANT_TYPE = "implicit";
-    private static String OAUTH_TOKEN_URL = "https://www.livecoding.tv/o/authorize/?client_id=" + CLIENT_ID + "&response_type=token&" + RANDOM_STATE;
-
-    WebView web;
-    Button auth;
-    SharedPreferences pref;
-    TextView Access;
-
     List<LiveStreamsOnAir> items;
-    Type listType = new TypeToken<List<LiveStreamsOnAir>>() {}.getType();
+    Type listType = new TypeToken<List<LiveStreamsOnAir>>() { }.getType();
 
     Context context;
 
@@ -79,129 +67,16 @@ public class MainActivity extends AppCompatActivity implements Callback<List<Liv
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
 
-        pref = getSharedPreferences("AppPref", MODE_PRIVATE);
-        Access = (TextView) findViewById(R.id.Access);
-        auth = (Button) findViewById(R.id.auth);
-        auth.setOnClickListener(new View.OnClickListener() {
-            Dialog auth_dialog;
-
-            @Override
-            public void onClick(View view) {
-
-                Log.e("URL", OAUTH_TOKEN_URL + "?redirect_uri=" + REDIRECT_URI + "&response_type=token&client_id" + CLIENT_ID);
-                auth_dialog = new Dialog(MainActivity.this);
-                auth_dialog.setContentView(R.layout.auth_dialog);
-                web = (WebView) auth_dialog.findViewById(R.id.webv);
-                web.getSettings().setJavaScriptEnabled(true);
-                web.loadUrl(OAUTH_TOKEN_URL + "?redirect_uri=" + REDIRECT_URI + "&response_type=token&client_id" + CLIENT_ID);
-                web.setWebViewClient(new WebViewClient() {
-
-                    boolean authComplete = false;
-                    Intent resultIntent = new Intent();
-
-                    @Override
-                    public void onPageStarted(WebView view1, String url, Bitmap favicon) {
-                        super.onPageStarted(view1, url, favicon);
-                        Log.e("page_started_url" , url);
-                    }
-
-                    String authCode;
-
-                    @Override
-                    public void onPageFinished(WebView view1, String url) {
-                        super.onPageFinished(view1, url);
-
-                        if (url.contains("?code=") && authComplete != true) {
-                            Uri uri = Uri.parse(url);
-                            authCode = uri.getQueryParameter("code");
-                            Log.i("", "CODE: " + authCode);
-                            authComplete = true;
-                            resultIntent.putExtra("code", authCode);
-                            MainActivity.this.setResult(Activity.RESULT_OK, resultIntent);
-                            setResult(Activity.RESULT_CANCELED, resultIntent);
-
-                            SharedPreferences.Editor edit = pref.edit();
-                            edit.putString("Code", authCode);
-                            edit.commit();
-                            auth_dialog.dismiss();
-                            new TokenGet().execute();
-                            Toast.makeText(getApplicationContext(), "Authorization code is: " + authCode, Toast.LENGTH_SHORT).show();
-                        } else if (url.contains("error_acccess_denied")) {
-                            Log.i("", "ACCESS_DENIED_HERE");
-                            resultIntent.putExtra("code", authCode);
-                            authComplete = true;
-                            setResult(Activity.RESULT_CANCELED, resultIntent);
-                            Toast.makeText(getApplicationContext(), "error occured", Toast.LENGTH_SHORT).show();
-
-                            auth_dialog.dismiss();
-                        }
-                    }
-                });
-                auth_dialog.show();
-                auth_dialog.setTitle("Authorize Livecoding");
-                auth_dialog.setCancelable(true);
-            }
-        });
-    }
-
-    private class TokenGet extends AsyncTask<String, String, JSONObject> {
-        private ProgressDialog pDialog;
-        String Code;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pDialog = new ProgressDialog(MainActivity.this);
-            pDialog.setMessage("Contacting Livecoding...");
-            pDialog.setIndeterminate(false);
-            pDialog.setCancelable(true);
-            Code = pref.getString("Code", "");
-            pDialog.show();
-        }
-
-        @Override
-        protected JSONObject doInBackground(String... args) {
-
-            AccessToken jParser = new AccessToken();
-            JSONObject json = jParser.gettoken(OAUTH_TOKEN_URL, Code, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, GRANT_TYPE);
-            return json;
-        }
-
-        @Override
-        protected void onPostExecute(JSONObject json) {
-            pDialog.dismiss();
-            if (json != null) {
-                try {
-                    String tok = json.getString("access_token");
-                    String expire = json.getString("expires_in");
-                    String refresh = json.getString("refresh_token");
-
-                    Log.d("Token Access", tok);
-                    Log.d("Expire", expire);
-                    Log.d("Refresh", refresh);
-                    auth.setText("Authenticated");
-                    Access.setText("Access Token:" + tok + "nExpires:" + expire + "nRefresh Token:" + refresh);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                Toast.makeText(getApplicationContext(), "Network Error", Toast.LENGTH_SHORT).show();
-                pDialog.dismiss();
-
-
-
-                Gson gson = new GsonBuilder()
-                        .create();
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("https://www.livecoding.tv/")
-                        .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().registerTypeAdapter(
-                                listType, new LiveStreamsOnAirDeserializer()).create()))
-                        .build();
-                LiveStreams_OnAir liveStreams_onAir = retrofit.create(LiveStreams_OnAir.class);
-                Call<List<LiveStreamsOnAir>> call = liveStreams_onAir.getData();
+        Gson gson = new GsonBuilder()
+                .create();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://www.livecoding.tv/")
+                .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().registerTypeAdapter(
+                        listType, new LiveStreamsOnAirDeserializer()).create()))
+                .build();
+        LiveStreams_OnAir liveStreams_onAir = retrofit.create(LiveStreams_OnAir.class);
+        Call<List<LiveStreamsOnAir>> call = liveStreams_onAir.getData();
 //        call.enqueue(this);
-            }
-        }
     }
 
     @Override
