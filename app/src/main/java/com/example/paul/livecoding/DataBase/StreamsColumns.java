ae37ab4044178d0c5aa2a4531e63afc071e0565d
Paul <paulgureghian@att.net>
2016-11-17 18:20:56 -0800

    11-17-16  6:15   PM

--- app/src/main/java/com/example/paul/livecoding/DataBase/StreamsColumns.java
+++ app/src/main/java/com/example/paul/livecoding/DataBase/StreamsColumns.java
@@ -1,5 +1,6 @@
 package com.example.paul.livecoding.DataBase;
 
+import android.renderscript.Element;
 
 import net.simonvt.schematic.annotation.AutoIncrement;
 import net.simonvt.schematic.annotation.DataType;
@@ -9,32 +10,73 @@ import net.simonvt.schematic.annotation.Unique;
 
 public class StreamsColumns {
 
-   @DataType(DataType.Type.TEXT) @PrimaryKey @AutoIncrement
+    @DataType(DataType.Type.TEXT)
+    @PrimaryKey
+    @AutoIncrement
     public static final String _URL = " _url";
-   @DataType(DataType.Type.TEXT) @NotNull
-   public static final String _USER = " _user"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String USER = "user";
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String USER_SLUG = "user_slug";
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String TITLE = "title";
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String DESCRIPTION = "description";
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String CODING_CATEGORY = "coding_category";
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String DIFFICULTY = "difficulty";
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String LANGUAGE = "language";
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String TAGS = "tags";
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String IS_LIVE = "is_live";
+    @DataType(DataType.Type.INTEGER)
+    @NotNull
+    public static final String VIEWERS_LIVE = "viewers_live";
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String VIEWING_URLS = "viewing_urls";
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String THUMBNAIL_URL = "thumbnail_url";
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String EMBED_URL = "embed_url";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
