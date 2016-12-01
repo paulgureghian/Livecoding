8f7dc27db5b7dce322e1bb7565da59532d389a38
Paul <paulgureghian@att.net>
2016-11-30 16:15:58 -0800

    11-30-16  4:15 PM

---
+++ app/src/main/java/com/example/paul/livecoding/SquareRelativeLayout/SquareRelativeLayout.java
@@ -0,0 +1,28 @@
+package com.example.paul.livecoding.SquareRelativeLayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class SquareRelativeLayout extends RelativeLayout {
+
+    public SquareRelativeLayout(Context context) {
+        this(context, null);
+    }
+
+    public SquareRelativeLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SquareRelativeLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widhtMeasureSpec, int heightMeasureSpec) {
+
+        int measureSpecSize = MeasureSpec.getSize(widhtMeasureSpec);
+        this.setMeasuredDimension(measureSpecSize, measureSpecSize);
+        super.onMeasure(widhtMeasureSpec, widhtMeasureSpec);
+    }
+}
