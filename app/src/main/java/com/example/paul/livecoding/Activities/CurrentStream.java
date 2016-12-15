7956e42dccf2b40eb9c2a8839adf5f5c04940123
Paul <paulgureghian@att.net>
2016-12-14 18:44:54 -0800

    12-14-16   6:40    PM

--- app/src/main/java/com/example/paul/livecoding/Activities/CurrentStream.java
+++ app/src/main/java/com/example/paul/livecoding/Activities/CurrentStream.java
@@ -19,15 +19,17 @@ import com.example.paul.livecoding.Adapter.StreamsAdapter;
 import com.example.paul.livecoding.DataBase.StreamsColumns;
 import com.example.paul.livecoding.DataBase.StreamsProvider;
 import com.example.paul.livecoding.R;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class CurrentStream extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, OnPreparedListener {
 
-    Context context;
-    EMVideoView emVideoView;
     int columnsId;
     Intent intent;
     Cursor mCursor;
+    Context context;
+    EMVideoView emVideoView;
     public StreamsAdapter streamsAdapter;
+    private FirebaseAnalytics mFirebaseAnalytics;
     private static final int CURSOR_LOADER_ID = 0;
 
     @Override
@@ -38,6 +40,9 @@ public class CurrentStream extends AppCompatActivity implements LoaderManager.Lo
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        mFirebaseAnalytics.setAnalyticsCollectionEnabled(true);
 
         context = this;
         emVideoView = (EMVideoView) findViewById(R.id.video_view);
@@ -78,6 +83,13 @@ public class CurrentStream extends AppCompatActivity implements LoaderManager.Lo
 
         DatabaseUtils.dumpCursor(data);
         Log.e("cursor", mCursor.getCount() + "");
+
+        String name = mCursor.getString(mCursor.getColumnIndex(StreamsColumns.TITLE));
+        String category = mCursor.getString(mCursor.getColumnIndex(StreamsColumns.CODING_CATEGORY));
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, name);
+        bundle.putString(FirebaseAnalytics.Param.ITEM_CATEGORY, category);
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.VIEW_ITEM, bundle);
     }
 
     @Override
