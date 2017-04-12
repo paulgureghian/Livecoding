a54e2fef9ff378eb44c2bedd5f3c85b3663dc872
Paul <paulgureghian@att.net>
2017-04-11 18:37:14 -0700

    4-11-17  6:30 PM

--- app/src/main/java/com/example/paul/livecoding/Service/LiveStreamsIntentService.java
+++ app/src/main/java/com/example/paul/livecoding/Service/LiveStreamsIntentService.java
@@ -136,6 +136,8 @@ public class LiveStreamsIntentService extends IntentService implements Callback<
                 contentValues.put(StreamsColumns.VIEWING_URLS1, item.getViewingUrls().get(0));
                 contentValues.put(StreamsColumns.VIEWING_URLS2, item.getViewingUrls().get(1));
                 contentValues.put(StreamsColumns.VIEWING_URLS3, item.getViewingUrls().get(2));
+            }else {
+                Toast.makeText(this, getResources().getString(R.string.no_connection_made), Toast.LENGTH_SHORT).show();
             }
 
             contentValues.put(StreamsColumns.THUMBNAIL_URL, item.getThumbnailUrl());
